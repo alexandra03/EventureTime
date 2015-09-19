3dd2f177d8234740333c409effe7774f5d649be5
Alexandra Sunderland <alexandra.sunderland@gmail.com>
2015-09-19 01:35:11 -0400

    sample url/view/template setup

--- eventuretime/events/views.py
+++ eventuretime/events/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader, Context
+
+
+def index(request):
+	template = loader.get_template('base.html')
+	context = Context({})
+
+	return HttpResponse(template.render(context))
+
+
+def event(request):
+	template = loader.get_template('event.html')
+	context = Context({})
+
+	return HttpResponse(template.render(context))
