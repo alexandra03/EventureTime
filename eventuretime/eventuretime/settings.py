75a834e2145afc91e945af6f5d19cae29fa12060
Amber Lee <amberlee@v1020-wn-13-92.campus-dynamic.uwaterloo.ca>
2015-09-19 13:28:01 -0400

    navbars implemented

--- eventuretime/eventuretime/settings.py
+++ eventuretime/eventuretime/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
